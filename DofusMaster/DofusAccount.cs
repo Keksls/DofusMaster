c4a404fc84d862445868ad71a14a45af78cbe322
Keks <bouetard.kevin@gmail.com>
2022-05-26 14:23:46 +0200

    first Push

---
+++ DofusMaster/DofusAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DofusMaster
+{
+    public class DofusAccount
+    {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr WindowHandle);
+        public const int SW_RESTORE = 9;
+
+        public IntPtr Handle;
+        public string Name;
+        public Process Process;
+
+        public DofusAccount(Process _process)
+        {
+            Process = _process;
+            Handle = Process.MainWindowHandle;
+            Name = Process.MainWindowTitle.Split(' ')[0];
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void ShowWindow()
+        {
+            SetForegroundWindow(Handle);
+        }
+    }
+}
---
+++ DofusMaster/Manager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DofusMaster
+{
+    public static class Manager
+    {
+        public static VirtualKeys NextKey { get; private set; }
+        public static VirtualKeys PreviewKey { get; private set; }
+        public static event Action NextKeyPressed;
+        public static event Action PreviewKeyPressed;
+        public static event Action<int> OnSelectAccount;
+        public static List<DofusAccount> ConnectedAccounts { get; private set; }
+        public static bool HasAccounts { get { return ConnectedAccounts != null && ConnectedAccounts.Count > 0; } }
+        private static int currentAccountIndex = 0;
+
+        static Manager()
+        {
+            currentAccountIndex = 0;
+            ConnectedAccounts = new List<DofusAccount>();
+            NextKey = VirtualKeys.Right;
+            PreviewKey = VirtualKeys.Left;
+            Keys.KeyDown += Keys_KeyDown;
+            NextKeyPressed += Manager_NextKeyPressed;
+            PreviewKeyPressed += Manager_PreviewKeyPressed;
+        }
+
+        private static void Manager_PreviewKeyPressed()
+        {
+            if (!HasAccounts)
+                return;
+            currentAccountIndex--;
+            if (currentAccountIndex < 0)
+                currentAccountIndex = ConnectedAccounts.Count - 1;
+            ConnectedAccounts[currentAccountIndex].ShowWindow();
+            OnSelectAccount?.Invoke(currentAccountIndex);
+        }
+
+        private static void Manager_NextKeyPressed()
+        {
+            if (!HasAccounts)
+                return;
+            currentAccountIndex++;
+            if (currentAccountIndex >= ConnectedAccounts.Count)
+                currentAccountIndex = 0;
+            ConnectedAccounts[currentAccountIndex].ShowWindow();
+            OnSelectAccount?.Invoke(currentAccountIndex);
+        }
+
+        private static void Keys_KeyDown(VirtualKeys key)
+        {
+            if (key == NextKey)
+                NextKeyPressed?.Invoke();
+            else if (key == PreviewKey)
+                PreviewKeyPressed?.Invoke();
+        }
+
+        public static void RefreshConnectedAccounts()
+        {
+            ConnectedAccounts = new List<DofusAccount>();
+            foreach (var process in Process.GetProcessesByName("Dofus"))
+                if (process.ProcessName == "Dofus")
+                    ConnectedAccounts.Add(new DofusAccount(process));
+        }
+    }
+}
